dc0cd82d246d097117ae99f2cdaca8cabe8aad81
Siriuo <siriuo@outlook.com>
2016-12-20 18:25:26 +0000

    Initial Version 0.1 commit.

---
+++ Wirenut Compiler/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.folderBrowser = New System.Windows.Forms.FolderBrowserDialog()
+        Me.folderOpenPath = New System.Windows.Forms.TextBox()
+        Me.folderOpenBrowse = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.folderSavePath = New System.Windows.Forms.TextBox()
+        Me.folderSaveBrowse = New System.Windows.Forms.Button()
+        Me.convertSave = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.progressBar = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'folderOpenPath
+        '
+        Me.folderOpenPath.Location = New System.Drawing.Point(12, 29)
+        Me.folderOpenPath.Name = "folderOpenPath"
+        Me.folderOpenPath.Size = New System.Drawing.Size(356, 20)
+        Me.folderOpenPath.TabIndex = 0
+        '
+        'folderOpenBrowse
+        '
+        Me.folderOpenBrowse.Location = New System.Drawing.Point(374, 29)
+        Me.folderOpenBrowse.Name = "folderOpenBrowse"
+        Me.folderOpenBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.folderOpenBrowse.TabIndex = 1
+        Me.folderOpenBrowse.Text = "Browse"
+        Me.folderOpenBrowse.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(215, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Browse for the folder containing all the mods"
+        '
+        'folderSavePath
+        '
+        Me.folderSavePath.Location = New System.Drawing.Point(12, 81)
+        Me.folderSavePath.Name = "folderSavePath"
+        Me.folderSavePath.Size = New System.Drawing.Size(356, 20)
+        Me.folderSavePath.TabIndex = 3
+        '
+        'folderSaveBrowse
+        '
+        Me.folderSaveBrowse.Location = New System.Drawing.Point(374, 81)
+        Me.folderSaveBrowse.Name = "folderSaveBrowse"
+        Me.folderSaveBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.folderSaveBrowse.TabIndex = 4
+        Me.folderSaveBrowse.Text = "Browse"
+        Me.folderSaveBrowse.UseVisualStyleBackColor = True
+        '
+        'convertSave
+        '
+        Me.convertSave.Location = New System.Drawing.Point(12, 110)
+        Me.convertSave.Name = "convertSave"
+        Me.convertSave.Size = New System.Drawing.Size(437, 49)
+        Me.convertSave.TabIndex = 5
+        Me.convertSave.Text = "Convert & Save"
+        Me.convertSave.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Choose the save location"
+        '
+        'progressBar
+        '
+        Me.progressBar.Location = New System.Drawing.Point(12, 165)
+        Me.progressBar.Name = "progressBar"
+        Me.progressBar.Size = New System.Drawing.Size(437, 23)
+        Me.progressBar.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 196)
+        Me.Controls.Add(Me.progressBar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.convertSave)
+        Me.Controls.Add(Me.folderSaveBrowse)
+        Me.Controls.Add(Me.folderSavePath)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.folderOpenBrowse)
+        Me.Controls.Add(Me.folderOpenPath)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents folderBrowser As FolderBrowserDialog
+    Friend WithEvents folderOpenPath As TextBox
+    Friend WithEvents folderOpenBrowse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents folderSavePath As TextBox
+    Friend WithEvents folderSaveBrowse As Button
+    Friend WithEvents convertSave As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents progressBar As ProgressBar
+End Class
---
+++ Wirenut Compiler/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System
+Imports System.IO
+Imports System.Collections
+Imports System.IO.Compression
+
+
+Public Class Form1
+    Private Sub folderOpenBrowse_Click(sender As Object, e As EventArgs) Handles folderOpenBrowse.Click
+        If (folderBrowser.ShowDialog() = DialogResult.OK) Then
+            folderOpenPath.Text = folderBrowser.SelectedPath
+        End If
+    End Sub
+
+    Private Sub folderSaveBrowse_Click(sender As Object, e As EventArgs) Handles folderSaveBrowse.Click
+        If (folderBrowser.ShowDialog() = DialogResult.OK) Then
+            folderSavePath.Text = folderBrowser.SelectedPath
+        End If
+    End Sub
+
+    Private Sub convertSave_Click(sender As Object, e As EventArgs) Handles convertSave.Click
+        Dim openPath As String = folderOpenPath.Text
+        Dim savePath As String = folderSavePath.Text
+
+        Dim modFiles As String() = Directory.GetFiles(openPath, "*.jar")
+
+        If (Not Directory.Exists(openPath + "/mods")) Then
+
+            Directory.CreateDirectory(openPath + "/mods")
+
+
+        End If
+
+        If (Not Directory.Exists(openPath + "/done")) Then
+
+            Directory.CreateDirectory(openPath + "/done")
+
+
+        End If
+
+        progressBar.Maximum = modFiles.Count
+
+
+        For Each modFile As String In modFiles
+            Dim fileName As String = Path.GetFileNameWithoutExtension(modFile)
+            Dim fileExtension As String = Path.GetExtension(modFile)
+
+
+            System.IO.File.Move(modFile, openPath + "/mods/" + fileName + fileExtension)
+            Console.WriteLine("Moved To Mods Folder")
+
+            If (Not File.Exists(savePath + "/" + fileName.ToLower + ".zip")) Then
+
+                ZipFile.CreateFromDirectory(openPath + "/mods", savePath + "/" + fileName.ToLower + ".zip", CompressionLevel.Fastest, True)
+                Console.WriteLine("Zipped File")
+            End If
+            System.IO.File.Move(openPath + "/mods/" + fileName + fileExtension, openPath + "/done/" + fileName + fileExtension)
+            Console.WriteLine("Moved To Done Folder")
+
+            progressBar.Value = progressBar.Value + 1
+            Console.WriteLine("Updated Progress Bar")
+        Next
+
+    End Sub
+End Class
